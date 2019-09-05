f8aa4b1c5414e3584a9f4ee2d002692cedc6a14c
LeonardoTomalo <leo.tomalo@gmail.com>
2019-09-05 05:52:36 -0500

    Commit Modificaciones: Se modifico el controlador de recursoPrincipal para que pueda presentar el mapa de gluon maps, falta agregar puntos en el mapa y modificar y adaptarlo para todos los eventos que soportara. Ademas se modifico el Launch del sistema para que pueda interactuar bien con gluonmaps. Se espera exitos y muchos logros con este nuevo avance ya que me costo mucho esta sencilles.

--- src/ec/com/sai/controllers/RecursoPrincipal.java
+++ src/ec/com/sai/controllers/RecursoPrincipal.java
@@ -1,5 +1,7 @@
 package ec.com.sai.controllers;
 
+import com.gluonhq.maps.MapPoint;
+import com.gluonhq.maps.MapView;
 import com.jfoenix.controls.JFXButton;
 
 import ec.com.sai.utilities.Context;
@@ -27,11 +29,42 @@ public class RecursoPrincipal {
 	public void initialize() {	
 		General.setContentParent("/viewPrincipal/RecursoInformacionBasica.fxml", anch_contenedor);
 		General.setContentParent("/viewBusqueda/BusquedaRecurso.fxml", anch_busqueda);
+		
+		MapView map = new MapView();
+//		anch_mapa.getChildren().removeAll();
+//		anch_mapa.getChildren().add(map);
+		
+		try{
+			if(!anch_mapa.equals(null))anch_mapa.getChildren().removeAll();
+			AnchorPane.setBottomAnchor(map, 00.0);
+			AnchorPane.setLeftAnchor(map, 00.0);
+			AnchorPane.setTopAnchor(map, 00.0);
+			AnchorPane.setRightAnchor(map, 00.0);
+			anch_mapa.getChildren().setAll(map);
+		}catch(Exception ex){
+			System.out.println(ex.getMessage());
+		}
+		
+		MapPoint mapPoint = new MapPoint(-2.403112, -80.680321);
+//		Node icon = new Circle(5, Color.RED);
+		
+		
+//		Node icon = new Circle(5, Color.BLUE);
+//		Point2D mapPoint = baseMap.getMapPoint(point.getLatitude(), point.getLongitude());
+//		icon.setTranslateX(mapPoint.getX());
+//		icon.setTranslateY(mapPoint.getY());
+		
+//		MapLayer layer = new MapLayer();			
+//        map.addLayer(layer);
+        map.setCenter(mapPoint);
+        map.setZoom(30);
+        map.flyTo(1., mapPoint, 2.);
+        
 	}        
 
     @FXML
     void crearRecurso(ActionEvent event) {
-    	Context.getInstance().setAnch_Contenido(anch_rp);    	
+    	Context.getInstance().setAnch_Contenido(anch_rp);
     	General.setContentParent("/viewRecurso/Recurso.fxml", (AnchorPane) anch_rp.getParent());
     }
 
@@ -43,7 +76,6 @@ public class RecursoPrincipal {
     @FXML
     void modificarRecurso(ActionEvent event) {
 //    	NotificationsTool.showNotificationPush();
-    	
     	
     }
 
