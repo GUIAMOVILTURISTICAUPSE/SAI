22734df0dafee93266f5f28df592f9a80b47d8e1
LeonardoTomalo <leo.tomalo@gmail.com>
2019-09-04 23:47:05 -0500

    Commit Module-Info: Se agrego requires para utilizar lombok, model.

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module guia_movil.sai {
 	exports ec.com.sai.main;
 	exports ec.com.sai.utilities;
 	exports ec.com.sai.controllers;
+	exports ec.com.sai.model;
 
 	requires com.jfoenix;
 	requires javafx.base;
@@ -9,6 +10,8 @@ module guia_movil.sai {
 	requires javafx.fxml;
 	requires transitive javafx.graphics;
 	requires org.controlsfx.controls;
+	requires lombok;
+	requires javafx.web;
 	
 	opens ec.com.sai.controllers to javafx.fxml;
 //	opens javafx.controls to com.jfoenix;
